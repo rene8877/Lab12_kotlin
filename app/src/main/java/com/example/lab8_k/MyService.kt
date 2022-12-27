92476033fb209c749ab5b13787b9c941cc02fba2
杜睿霖 <pig2709008@gmail.com>
2022-12-27 22:12:43 +0800

    coroutines

--- app/src/main/java/com/example/lab8_k/MyService.kt
+++ app/src/main/java/com/example/lab8_k/MyService.kt
@@ -3,12 +3,23 @@ package com.example.lab8_k
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import kotlinx.coroutines.*
 
 class MyService : Service() {
     fun MyService() {}
     override fun onCreate() {
         super.onCreate()
-        Thread {
+        GlobalScope.launch(Dispatchers.Main){
+            try {
+                delay(5000)
+                val intent = Intent(this@MyService, MainActivity2::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                this@MyService.startActivity(intent)
+            } catch (e: InterruptedException) {
+                e.printStackTrace()
+            }
+        }
+   /*     Thread {
             try {
                 Thread.sleep(5000)
                 val intent = Intent(this, MainActivity2::class.java)
@@ -18,7 +29,7 @@ class MyService : Service() {
                 e.printStackTrace()
             }
         }.start()
-
+*/
     }
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         return super.onStartCommand(intent, flags, startId)
